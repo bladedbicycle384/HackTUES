7f9cbfd31d6e00b8011166639f6c7904a726f5cd
alexander24032003 <36775978+alexander24032003@users.noreply.github.com>
2018-03-16 17:46:22 +0200

    Update project3.py

--- project3.py
+++ project3.py
@@ -1,11 +1,20 @@
-class User:
-    def __init__(self, income):
-        self.income = income
+from tkinter import *
 
-user = User(float(input("Your income:")))
-decision = str(input("What do you want to do?(Sell/Rent/Chance)"))
+root = Tk()
 
-if decision == "Rent":        
+def Decision(decision):
+    if decision == "Rent":
+        w = Label(root, Rent(decision))
+        w.pack()
+    elif decision == "Sell":
+        w = Label(root, Sell(decision))
+        w.pack()
+    elif decision == "Chance":
+        w = Label(root, Chance(decision))
+        w.pack()
+
+
+def Rent(decision):
     purchasing_area = float(input("Price of the area in euro:"))
     construction_price = float(input("Price of the construction in euro:"))
     area = float(input("Enter the area in m*m:"))
@@ -16,44 +25,73 @@ if decision == "Rent":
     x = (user.income-residue_after_payments)/income_for_month
     construction_maintenance = float(input("Price of the construction maintenance for month in euro:"))
     residue_after_payments = user.income-construction_price-purchasing_area
-    if residue_after_payments >= 0:    
-        print("There are", residue_after_payments, "euros left!")
-		
-    else:
-        print("You must pay", 0-residue_after_payments, "euro!")
-
-    print("You can make the payment after:", int(x))
-    print('Income for month is', income_for_month)
-    print("The summarized area is", construction_area)
+    if residue_after_payments >= 0:
+        w = Label(root,residue_after_payments, text = "There are")
+        w.pack()
+        w = Label(root, text = "euros left!")
+        w.pack()
 	
-elif decision == "Sell":
+    else:
+        q = Label("You must pay", 0-residue_after_payments, "euro!")
+        q.pack()
+        e = Label(root,int(x), text = "You can make the payment after:")
+        e.pack()
+        r = Label(root,income_for_month, text = 'Income for month is')
+        r.pack()
+        t = Label(root,construction_area, text = "The summarized area is")
+        t.pack()
+
+def Sell(decision):
     purchasing_area1 = float(input("Price of the area in euro:"))
     construction_price1 = float(input("Price of the construction in euro:"))
-    income_after_payments1 = user.income-construction_price-purchasing_area
+    income_after_payments1 = user.income-construction_price1-purchasing_area1
     selling_area = float(input("Price of the selling area:"))
     gains = selling_area-purchasing_area1-construction_price1
-    print("Your gains are", gains)
-
-elif decision == "Chance":
+    y = Label(root, text = "Your gains are")
+    y.pack()
+    y = Label(root, text = gains)
+    y.pack()
+def Chance(decision):
     percentage_of_sellings = float(input("The percentage of the sellings(The part of your possessions you want to sell.):"))
     percentage_of_rents = float(input("The percentage of the rents(The part of things(for example: apartments)you want to rent.):"))
-    construction_price = float(input("Price of the construction in euro:"))
-    purchasing_area1 = float(input("Price of the area in euro:"))
-    purchasing_area = float(input("Price of the area in euro:"))
-    selling_area = float(input("Price of the selling area:"))
     construction_price1 = float(input("Price of the construction in euro:"))
-    residue_after_payments = user.income-construction_price-purchasing_area
+    purchasing_area1 = float(input("Price of the area in euro:"))
+    selling_area = float(input("Price of the selling area:"))
+    residue_after_payments = user.income-construction_price1-purchasing_area1
     gains = selling_area-purchasing_area1-construction_price1
-    earnings = (percentage_of_sellings/100)*gains - (percentage_of_rents/100)*residue_after_payments	
+    earnings = (percentage_of_rents/100)*residue_after_payments - (percentage_of_sellings/100)*gains
 	
-if earnings > 0:
-    print(earnings)
-    print("Do it if you want!")
+    if earnings > 0:
+        u = Label(root,text = "You will receive")
+        u.pack()
+        u = Label(root, text = earnings)
+        u.pack()
+        o = Label(root, text = "Do it if you want!")
+        o.pack()
+        
+    elif earnings == 0:
+        p = Label(root, text = earnings)
+        p.pack()
+        p = Label(root, text = "You will not receive anything!")
+        p.pack()
+        a = Label(root, text = "There is no point of doing this!")
+        a.pack()
+	
+    else:
+        s = Label(root, text = "You must pay")
+        s.pack()
+        s = Label(root, text = 0 - earnings)
+        s.pack()
+        s = Label(root, text = "euro!")
+        s.pack()
+        d = Label(root, text = "Do NOT do this!")
+        d.pack()
 
-elif earnings == 0:
-	print(earnings)
-	print("There is no point of doing this!")
-	
-else:
-	print("You must pay", 0 - earnings, "euro!")
-print("Do NOT do this!")
+class User:
+    def __init__(self, income):
+        self.income = income
+
+user = User(float(input("Your income:")))
+decision = input("What do you want to do?(Sell/Rent/Chance)")
+a = Label(root, Decision(decision))
+a.pack()
